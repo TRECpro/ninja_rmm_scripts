0d03e5d65707b57b0d2f0aa83a64eff19c009e15
jgerman03 <josh@aplg8.net>
2018-10-15 13:00:52 -0400

    Update Add-Local Admin.ps1

---
+++ Add-Local Admin.ps1
@@ -0,0 +1,34 @@
+﻿##wrote by jgerman03 - use this script to create a new user account, set as a local administrator, with password, and hidden from login (if needed) ##
+
+## Change the following as needed "username" and "password" ##
+## Run this schedule on demand or a scheduled basis within NinjaRMM ##
+
+$Username = "admin"
+$Password = "password"
+
+$group = "Administrators"
+
+$adsi = [ADSI]"WinNT://$env:COMPUTERNAME"
+$existing = $adsi.Children | where {$_.SchemaClassName -eq 'user' -and $_.Name -eq $Username }
+
+if ($existing -eq $null) {
+
+    Write-Host "Creating new local user $Username."
+    & NET USER $Username $Password /add /y /expires:never
+
+    Write-Host "Adding local user $Username to $group."
+    & NET LOCALGROUP $group $Username /add
+
+}
+else {
+    Write-Host "Setting password for existing local user $Username."
+    $existing.SetPassword($Password)
+}
+
+## Sets password as never expires ##
+Write-Host "Ensuring password for $Username never expires."
+& WMIC USERACCOUNT WHERE "Name='$Username'" SET PasswordExpires=FALSE
+
+## Hides User Account from Login page ##
+$path = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList'
+New-Item $path | New-ItemProperty -Name admin -Value 0 -PropertyType DWord
