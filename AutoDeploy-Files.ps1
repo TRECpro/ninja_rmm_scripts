f4b220ce089d842c9ca3e95ce6206c448419adb1
jgerman03 <josh@aplg8.net>
2018-10-15 23:42:43 -0400

    Update AutoDeploy-Files.ps1

--- AutoDeploy-Files.ps1
+++ AutoDeploy-Files.ps1
@@ -1,9 +1,9 @@
-## Cleans Directory before script runs ##
+## Cleans Directory before script runs 
 Remove-Item c:\tools\*.*
 
-## Download deployment tools from internet based file repo (filecloud, dropbox, S3, etc) ##
-## URL = internet accessible address for downloads ##
-## Output = local destination ##
+## Download deployment tools from internet based file repo (filecloud, dropbox, S3, etc) 
+## URL = internet accessible address for downloads #
+## Output = local destination 
 
 $url = "http://aplg8.net/repo/aistools.zip"
 $output = "c:\tools\ais-tools.zip"
@@ -15,9 +15,9 @@ Start-BitsTransfer -Source $url -Destination $output
 
 Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 
-## Extracts aistools.zip to c:\tools\ais-tools\* ##
+## Extracts aistools.zip to c:\tools\ais-tools\*
 expand-archive -path 'C:\tools\ais-tools.zip' -destinationpath 'C:\tools\ ' -Force
 
 
-## Cleans up downloaded .zip file ##
+## Cleans up downloaded .zip file
 Remove-Item c:\tools\*.zip
