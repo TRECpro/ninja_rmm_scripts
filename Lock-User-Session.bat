6e3c3cd8f84a4c636e54d1a8a1d7e17755988367
jgerman03 <josh@aplg8.net>
2018-10-15 23:55:28 -0400

    Update Lock-User-Session.bat

--- Lock-User-Session.bat
+++ Lock-User-Session.bat
@@ -1,4 +1,4 @@
-# Simple script that invokes a lock of the current workstation for security purposes like user termination
+# Simple .bat script that invokes a lock of the current workstation for security purposes like user termination
 # Set to run as SYSTEM on an on-demand basis
 
 rundll32.exe user32.dll, LockWorkStation
